bb13f58fe209dea08fd65eb1f1d8e2a4628622e6
amminadab <amminadabeliasfant@gmail.com>
2023-06-05 20:29:41 +0300

    learned how counter works

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-      name: "Amminadab",
-      age: 22,
-      img: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Mercedes-Benz/Mercedes-Benz-G-Class/6607/front-left-side-47.jpg?imwidth=210&impolicy=resize",
-      inputValue: "",
+      counter: 0,
     };
   },
   methods: {
-    ageInFiveYears() {
-      return 5 + this.age;
+    add() {
+      this.counter += 1;
     },
-    favoriteNumber() {
-      const randomNumber = Math.random();
-      if (randomNumber > 0.5) {
-        return 1;
-      } else {
-        return 0;
-      }
+    reduce() {
+      this.counter -= 1;
     },
   },
 });
 
-app.mount("#assignment");
+app.mount("#events");
