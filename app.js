e7e47099d6c8f5fc8496b6c5f6d94dc2c3a72823
amminadab <amminadabeliasfant@gmail.com>
2023-06-05 20:40:01 +0300

    dynamic counter on vue

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      number: 10,
     };
   },
   methods: {
-    add() {
-      this.counter += 1;
+    add(num) {
+      this.counter += num;
     },
-    reduce() {
-      this.counter -= 1;
+    reduce(num) {
+      this.counter -= num;
     },
   },
 });
